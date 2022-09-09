38416c7448284f6f94515e3b2e4eb67ab42c8374
zack1431 <zakrshkh16@gmail.com>
2022-09-09 19:34:45 +0530

    api updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
     let writer = fs.createWriteStream('uploadfolder/current_date_time.txt') 
     let date = new Date;
     writer.write('Date & Time: '+date);
-    res.send(date)
+    res.send('<a href="uploadfolder/current_date_time.txt" target="_blank" download>Download Text File</a>')
 });
 var server = app.listen(process.env.PORT || 8085, () => {
     var port = server.address().port;
